4c9607dd4540b91d6af588318d7683a3889f010c
Alex Handzhiev <alex.handzhiev@gmail.com>
2020-02-19 12:06:00 +0200

    Another Major Update

--- src/com/whitehatgaming/game/BoardState.java
+++
@@ -1,5 +0,0 @@
-package com.whitehatgaming.game;
-
-public enum BoardState {
-    STANDARD, CHECK
-}
--- src/main/tests/BoardEvaluatorTest.java
+++ src/main/tests/BoardEvaluatorTest.java
@@ -1,4 +1,23 @@
 package main.tests;
 
+import com.whitehatgaming.game.Board;
+
 public class BoardEvaluatorTest {
+    private Board board;
+
+    @Befo
+    public void setUp() {
+        board = new Board();
+    }
+
+    @Tes(expected = IllegalStateException.class)
+    public void kingLessBoard() {
+        BoardEvaluator.findKing(Color.WHITE, board);
+    }
+
+    @Test()
+    public void initialLegalMoves() {
+        new StandardInitializer().init(board);
+        assertEquals(20, BoardEvaluator.legalMoves(Color.WHITE, null, board).size());
+    }
 }
